d4eacafad2899b31551b351137d541db2b43c1e9
Galfest <abramyants90@mail.ru>
2023-10-12 14:54:26 +0300

    4

--- src/com/gb/java/hw/hw1/ChatClient.java
+++ src/com/gb/java/hw/hw1/ChatClient.java
@@ -1,15 +1,12 @@
-package com.gb.java.hw1;
+package com.gb.java.hw.hw1;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.Socket;
 
 public class ChatClient extends JFrame {
--- src/com/gb/java/hw/hw1/ChatServer.java
+++ src/com/gb/java/hw/hw1/ChatServer.java
@@ -1,4 +1,4 @@
-package com.gb.java.hw1;
+package com.gb.java.hw.hw1;
 
 import javax.swing.*;
 import java.awt.*;
