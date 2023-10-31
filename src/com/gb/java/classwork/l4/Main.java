72a1b8bdd47556150c0a1e05e5627b17ad6f201e
mac <abramyants90@mail.ru>
2023-10-31 20:27:30 +0300

    mac

--- src/com/gb/java/classwork/l4/Main.java
+++ src/com/gb/java/classwork/l4/Main.java
@@ -1,9 +1,26 @@
 package com.gb.java.classwork.l4;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Collection<Integer> collection = List.of(1,2,3,4,5,6);
+        //Iterator
+        Iterator<Integer> iterator = collection.iterator();
+        while (iterator.hasNext()){
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+        //For each
+        for (Integer value : collection){
+            System.out.print(value + " ");
+        }
+        System.out.println();
+        //Stream
+        collection.stream().forEach(System.out::print);
     }
 
 }
--- src/draft/Collectionlesson.java
+++ src/draft/Collectionlesson.java
@@ -8,12 +8,19 @@ public class Collectionlesson {
 
     public static void main(String[] args) {
         Collection<Integer> collection = List.of(1,2,3,4,5,6);
-
+        //Iterator
         Iterator<Integer> iterator = collection.iterator();
         while (iterator.hasNext()){
             System.out.print(iterator.next() + " ");
         }
         System.out.println();
+        //For each
+        for (Integer value : collection){
+            System.out.print(value + " ");
+        }
+        System.out.println();
+        //Stream
+        collection.stream().forEach(System.out::print);
     }
 
 }
